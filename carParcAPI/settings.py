8716d4ee6d511b3f2ede9a89d5e81fb224f7d0f2
sappi <55806139+thieriKhan@users.noreply.github.com>
2024-12-26 09:50:29 +0100

    vercel

--- carParcAPI/settings.py
+++ carParcAPI/settings.py
@@ -149,6 +149,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 
